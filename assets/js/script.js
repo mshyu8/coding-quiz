f28b05a5bca6f90818e3d59da556ae3ff65d18dd
Michael Shyu <michaelshyu11@gmail.com>
2022-11-12 00:54:55 -0500

    javascript additions including timer changes by correct and wrong

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,90 +4,9 @@ var card = document.querySelector(".card");
 var h1 = document.querySelector("#h1");
 var h2 = document.querySelector("#h2");
 var timerCount = 75;
-
-//function hide(event) {
-    //    event.currentTarget.style.visibility = 'hidden';
-    //    console.log("wassuop");
-    //    console.log("wassup");
-    //     var question1 = document.createElement('div');
-    //    question1.setAttribute('id', 'question1');
-    //    card.appendChild(question1);
-//    var q1 = document.createElement('h2');
-//    q1.setAttribute('id', 'q1');
-//    question1.appendChild(q1);
-//    q1.textContent = "Commonly used data types DO Not include: ";
-
-//}
-
-//startButton.addEventListener("click", hide, false);
+var h4 = document.querySelector('#status');
 
 
-function question1(event) {
-    h1.textContent = "Commonly used data types DO Not include";
-    h2.textContent = "";
-    realStart.remove();
-    var answer1 = document.createElement('button');
-    answer1.setAttribute('id', 'answer1');
-    h2.appendChild(answer1);
-    answer1.textContent = "1. Strings";
-    var answer2 = document.createElement('button');
-    answer2.setAttribute('id', 'answer2');
-    h2.appendChild(answer2);
-    answer2.textContent = "2. Booleans";
-    var answer3 = document.createElement('button');
-    answer3.setAttribute('id', 'answer3');
-    h2.appendChild(answer3);
-    answer3.textContent = "3. Alerts";
-    var answer4 = document.createElement('button');
-    answer4.setAttribute('id', 'answer4');
-    h2.appendChild(answer4);
-    answer4.textContent = "4. Numbers";
-
-    
-    startButton.addEventListener("click", question2);
-}
-
-function question2(event) {
-    h1.textContent = "The condition in an if / else statement is enclosed with __________.";
-    answer1.textContent = "1. Quotes";
-    answer2.textContent = "2. Curly Brackets";
-    answer3.textContent = "3. Parenthesis";
-    answer4.textContent = "4. Square Brackets";
-    startButton.addEventListener("click", question3);
-
-    if (answer1) {
-        console.log("yes");
-    } else {
-        console.log("no");
-    }
-}
-
-function question3(event) {
-    h1.textContent = "Arrays in JavaScript can be used to store ______";
-    answer1.textContent = "1. Numbers and Strings";
-    answer2.textContent = "2. Other Arrays";
-    answer3.textContent = "3. Booleans";
-    answer4.textContent = "4. All of the above";
-    startButton.addEventListener("click", question4);
-}
-
-function question4(event) {
-    h1.textContent = "Strings values must be encolsed within ______ when being assigned to variables.";
-    answer1.textContent = "1. Commas";
-    answer2.textContent = "2. Curly Brackets";
-    answer3.textContent = "3. Quotes";
-    answer4.textContent = "4. Parenthesis";
-    startButton.addEventListener("click", question5);
-}
-
-function question5(event) {
-    h1.textContent = "A very useful tool used during development and devugging for printing content to the debugger is: ";
-    answer1.textContent = "1. JavaScript";
-    answer2.textContent = "2. Terminal/Bash";
-    answer3.textContent = "3. For loops";
-    answer4.textContent = "4. Console.log";
-    startButton.addEventListener("click", endScreen);
-}
 
 function endScreen(event) {
     h1.textContent = "All done!";
@@ -113,11 +32,7 @@ function startTimer() {
   }
 
 
-//realStart.addEventListener("click", startTimer);
 
-
-
-//startButton.addEventListener("click", question1);
 
 function start() {
     if (realStart) {     
@@ -139,16 +54,184 @@ function start() {
         var answer4 = document.createElement('button');
         answer4.setAttribute('id', 'answer4');
         h2.appendChild(answer4);
-        answer4.textContent = "4. Numbers";  
+        answer4.textContent = "4. Numbers";      
+        answer1.addEventListener("click", wrong1);  
+        answer2.addEventListener("click", correct1);
+        answer3.addEventListener("click", wrong1);
+        answer4.addEventListener("click", wrong1);
+        startTimer();
     } 
 }
 
-function q1() {
-    if (answer1) {
-        console.log("lol yah");
-    } else if (answer2) {
-        console.log("lol noooo");
-    }
+
+function q2() {
+    h1.textContent = "The condition in an if / else statement is enclosed with __________.";
+    h2.textContent = "";
+    realStart.remove();
+    var answer1 = document.createElement('button');
+    answer1.setAttribute('id', 'answer1');
+    h2.appendChild(answer1);
+    answer1.textContent = "1. Quotes";
+    var answer2 = document.createElement('button');
+    answer2.setAttribute('id', 'answer2');
+    h2.appendChild(answer2);
+    answer2.textContent = "2. Curly Brackets";
+    var answer3 = document.createElement('button');
+    answer3.setAttribute('id', 'answer3');
+    h2.appendChild(answer3);
+    answer3.textContent = "3. Parenthesis";
+    var answer4 = document.createElement('button');
+    answer4.setAttribute('id', 'answer4');
+    h2.appendChild(answer4);
+    answer4.textContent = "4. Square Brackets";      
+    answer1.addEventListener("click", wrong2);  
+    answer2.addEventListener("click", correct2);
+    answer3.addEventListener("click", wrong2);
+    answer4.addEventListener("click", wrong2);
+} 
+
+function q3() {
+    h1.textContent = "Arrays in JavaScript can be used to store ______.";
+    h2.textContent = "";
+    realStart.remove();
+    var answer1 = document.createElement('button');
+    answer1.setAttribute('id', 'answer1');
+    h2.appendChild(answer1);
+    answer1.textContent = "1. Numbers and Strings";
+    var answer2 = document.createElement('button');
+    answer2.setAttribute('id', 'answer2');
+    h2.appendChild(answer2);
+    answer2.textContent = "2. Other Arrays";
+    var answer3 = document.createElement('button');
+    answer3.setAttribute('id', 'answer3');
+    h2.appendChild(answer3);
+    answer3.textContent = "3. Booleans";
+    var answer4 = document.createElement('button');
+    answer4.setAttribute('id', 'answer4');
+    h2.appendChild(answer4);
+    answer4.textContent = "4. All of the above";      
+    answer1.addEventListener("click", wrong3);  
+    answer2.addEventListener("click", correct3);
+    answer3.addEventListener("click", wrong3);
+    answer4.addEventListener("click", wrong3);
+} 
+
+function q4() {
+    h1.textContent = "Strings values must be encolsed within ______ when being assigned to variables.";
+    h2.textContent = "";
+    realStart.remove();
+    var answer1 = document.createElement('button');
+    answer1.setAttribute('id', 'answer1');
+    h2.appendChild(answer1);
+    answer1.textContent = "1. Commas";
+    var answer2 = document.createElement('button');
+    answer2.setAttribute('id', 'answer2');
+    h2.appendChild(answer2);
+    answer2.textContent = "2. Curly Brackets";
+    var answer3 = document.createElement('button');
+    answer3.setAttribute('id', 'answer3');
+    h2.appendChild(answer3);
+    answer3.textContent = "3. Quotes";
+    var answer4 = document.createElement('button');
+    answer4.setAttribute('id', 'answer4');
+    h2.appendChild(answer4);
+    answer4.textContent = "4. Parenthesis";      
+    answer1.addEventListener("click", wrong4);  
+    answer2.addEventListener("click", correct4);
+    answer3.addEventListener("click", wrong4);
+    answer4.addEventListener("click", wrong4);
+} 
+
+function q5() {
+    h1.textContent = "A very useful tool used during development and devugging for printing content to the debugger is: ";
+    h2.textContent = "";
+    realStart.remove();
+    var answer1 = document.createElement('button');
+    answer1.setAttribute('id', 'answer1');
+    h2.appendChild(answer1);
+    answer1.textContent = "1. JavaScript";
+    var answer2 = document.createElement('button');
+    answer2.setAttribute('id', 'answer2');
+    h2.appendChild(answer2);
+    answer2.textContent = "2. Terminal/Bash";
+    var answer3 = document.createElement('button');
+    answer3.setAttribute('id', 'answer3');
+    h2.appendChild(answer3);
+    answer3.textContent = "3. For loops";
+    var answer4 = document.createElement('button');
+    answer4.setAttribute('id', 'answer4');
+    h2.appendChild(answer4);
+    answer4.textContent = "4. Console.log";      
+    answer1.addEventListener("click", wrong5);  
+    answer2.addEventListener("click", correct5);
+    answer3.addEventListener("click", wrong5);
+    answer4.addEventListener("click", wrong5);
+} 
+
+
+
+
+
+function correct1() {
+    q2();
+    h4.textContent = ("Correct");
+    timerCount += 10;
 }
+
+function wrong1() {
+    q2();
+    h4.textContent= ("Wrong");
+    timerCount += -20;
+}
+
+function correct2() {
+    q3();
+    h4.textContent = ("Correct");
+    timerCount += 10;
+}
+
+function wrong2() {
+    q3();
+    h4.textContent = ("Wrong");
+    timerCount += -20;
+}
+
+function correct3() {
+    q4();
+    h4.textContent = ("Correct");
+    timerCount += 10;
+}
+
+function wrong3() {
+    q4();
+    h4.textContent= ("Wrong");
+    timerCount += -20;
+}
+
+function correct4() {
+    q5();
+    h4.textContent = ("Correct");
+    timerCount += 10;
+}
+
+function wrong4() {
+    q5();
+    h4.textContent= ("Wrong");
+    timerCount += -20;
+}
+
+function correct5() {
+    endScreen();
+    h4.textContent = ("Correct");
+    timerCount += 10;
+}
+
+function wrong5() {
+    endScreen();
+    h4.textContent= ("Wrong");
+    timerCount += -20;
+}
+
+
 
 realStart.addEventListener("click", start);
